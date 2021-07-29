3606290f5b7345e0e582f391cab40fd7474155c4
Glitch (p5-js-template-cssi-2020) <none>
2021-07-29 22:33:17 +0000

    🌋🚅 Checkpoint ./script.js:19167394/312

--- script.js
+++ script.js
@@ -5,15 +5,31 @@
           mouseX, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, 
           keyCode, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
 
-let backgroundColor;
+let backgroundColor,ball;
 
 function setup() {
   // Canvas & color settings
   createCanvas(400, 400);
   colorMode(HSB, 360, 100, 100);
   backgroundColor = 95;
+  ball = new ball();
+  
 }
 
 function draw() {
   background(backgroundColor);
+  ball.moveSelf();
+  ball.showSelf();
 }
+class Ball{
+  constructor(){
+    this.x = 100;
+    this.y = 10;
+    this.velocity =1;
+    this.accerlartion=0.5;
+  }
+ moveSelf(){
+   this.y += this.velocity;
+   this.velocity += this.accerlartion;
+ }
+  showSelf
