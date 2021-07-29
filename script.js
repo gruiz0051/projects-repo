23839ef6d037e1b9e83b1b1a861b8941ed2fd49c
Glitch (p5-js-template-cssi-2020) <none>
2021-07-29 23:31:35 +0000

    🙈🍍 Checkpoint ./script.js:19167394/5611

--- script.js
+++ script.js
@@ -1,46 +1,122 @@
 // Be sure to name any p5.js functions we use in the global so Glitch can recognize them.
-// Add to this list as you consult the p5.js documentation for other functions. 
+// Add to this list as you consult the p5.js documentation for other functions.
 /* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
           rect, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, 
           mouseX, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, 
-          keyCode, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
-let bird;
+          keyCode,frameCount,key, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
+let bird,pipes;
+let pipe = [];
 
-function Bird(){
-  this.y = height/2;
+function Bird() {
+  this.y = height / 2;
   this.x = 64;
-  
-  this.gravity = 1;
-  this.velocity=0;
-  
-  this.show = function(){
+
+  this.gravity = 0.7;
+  this.lift = -12;
+  this.velocity = 0;
+
+  this.show = function() {
     fill(255);
-    ellipse(this.x,this.y,32,32);
-  }
-  
-  this.update=function(){
+    ellipse(this.x, this.y, 32, 32);
+  };
+
+  this.up = function() {
+    this.velocity += this.lift;
+  };
+
+  this.update = function() {
     this.velocity += this.gravity;
+    // this.velocity *= 0.9;
     this.y += this.velocity;
-    
-    if(this.y > height){
-      this.y=height;
-      this.velocity=0;
+
+    if (this.y > height) {
+      this.y = height;
+      this.velocity = 0;
     }
-    if(this.y < height){
-      this.y=height;
-      this.velocity=0;
+
+    if (this.y < 0) {
+      this.y = 0;
+      this.velocity = 0;
     }
-  }
+  };
 }
 
+
 function setup() {
-  createCanvas(400,600);
+  createCanvas(640, 480);
   bird = new Bird();
-  
+  pipes.push(new Pipe());
 }
 
 function draw() {
   background(0);
+
+  for (var i = pipes.length - 1; i >= 0; i--) {
+    pipes[i].show();
+    pipes[i].update();
+
+    if (pipes[i].hits(bird)) {
+      console.log('HIT');
+    }
+
+    if (pipes[i].offscreen()) {
+      pipes.splice(i, 1);
+    }
+  }
+
   bird.update();
   bird.show();
-}  
+
+  if (frameCount % 75 == 0) {
+    pipes.push(new Pipe());
+  }
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    bird.up();
+    //console.log("SPACE");
+  }
+}
+function Pipe() {
+  this.spacing = 175;
+  this.top = random(height / 6, (3 / 4) * height);
+  this.bottom = height - (this.top + this.spacing);
+  this.x = width;
+  this.w = 80;
+  this.speed = 6;
+
+  this.highlight = false;
+
+  this.hits = function(bird) {
+    if (bird.y < this.top || bird.y > height - this.bottom) {
+      if (bird.x > this.x && bird.x < this.x + this.w) {
+        this.highlight = true;
+        return true;
+      }
+    }
+    this.highlight = false;
+    return false;
+  };
+
+  this.show = function() {
+    fill(255);
+    if (this.highlight) {
+      fill(255, 0, 0);
+    }
+    rect(this.x, 0, this.w, this.top);
+    rect(this.x, height - this.bottom, this.w, this.bottom);
+  };
+
+  this.update = function() {
+    this.x -= this.speed;
+  };
+
+  this.offscreen = function() {
+    if (this.x < -this.w) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+}
