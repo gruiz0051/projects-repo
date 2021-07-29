9259070b727f9fb75512964ee0a309efd1530721
Glitch (p5-js-template-cssi-2020) <none>
2021-07-29 22:56:17 +0000

    👬🎯 Checkpoint ./script.js:19167394/2588

--- script.js
+++ script.js
@@ -5,42 +5,52 @@
           mouseX, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, 
           keyCode, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
 
-let backgroundColor,ball;
+let backgroundColor,balls;
 
 function setup() {
   // Canvas & color settings
   createCanvas(400, 400);
   colorMode(HSB, 360, 100, 100);
   backgroundColor = 95;
-  ball = new Ball();
+  balls = [];
+  for (let i = 0; i < 100; i++) {
+    balls.push(new Ball());
+  }
   
 }
 
 function draw() {
   background(backgroundColor);
+  for(let ball of balls){
   ball.moveSelf();
   ball.showSelf();
-}
-class Ball{
-  constructor(){
-    this.x = 100;
-    this.y = 10;
-    this.velocity = 1;
-    this.accerlartion=0.5;
-    this.size = 20;
   }
- moveSelf(){
-   this.y += this.velocity;
-   //this.velocity += this.accerlartion;
-   if(this.y > height - this.size/2){
-     this.velocity *= -1;
-   }else{
-     this.velocity += this.accerlartion;
-   }
- }
-  showSelf(){
-    fill(0);
-    ellipse(this.x,this.y,20,20)
+}
+
+class Ball {
+  constructor() {
+    this.size = random(10, 20);
+    // Make sure the ball fits fully onto the canvas.
+    this.x = random(this.size/2, width-this.size/2);
+    // At least some starting height.
+    this.y = random(height-100);
+    this.velocity = random(2);
+    this.acceleration = random(1);
+  }
+  moveSelf() {
+    // Increase y by velocity every moveSelf();
+    this.y += this.velocity;
     
+    // When at bottom edge, reverse the direction.
+    if (this.y >= height - this.size/2) {
+      this.velocity *= -1;
+    } else if (this.velocity <= 10) {
+        // Add constant Acceleration to velocity every moveSelf();
+        this.velocity += this.acceleration;
+    }
+  }
+  showSelf() {
+    fill(0);
+    ellipse(this.x,this.y,this.size,this.size);
   }
 }
