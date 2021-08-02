a3ca59a7426d506e5f816fde79b36a2b0844c2d9
Glitch (p5-js-template-cssi-2020) <none>
2021-08-02 23:09:52 +0000

    🐩🏁 Checkpoint ./script.js:19167394/790 ./.glitch-assets:19167394/373

--- script.js
+++ script.js
@@ -3,27 +3,18 @@
 /* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
           rect, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, 
           mouseX, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, 
-          keyCode,pipes,frameCount,key, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize *
+          keyCode,pipes,circle,frameCount,key, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize *
 */
-
+let imgPosition,img;
 function setup() {
   createCanvas(640, 480);
+  imgPosition = { x: 250, y: 250 };
 }
 
 function draw() {
-  background(255,0,0)
+  background(0)
+ image(imgPosition.x, imgPosition.y, 70)
 }
-class Cat{
-  constructor(){
-  this.size = 10;
-    this.d = 30;
-    this.x = 30;
-    this.y = 30;
-}
-
-  showSelf(){
-		noStroke();
-		fill(0);
-		ellipse(this.x, this.y, this.d );
-  }
+function preload() {
+  img = loadImage('https://cdn.glitch.com/d125e789-8ed6-42c4-8d94-84907c5535a6%2Fcat%20head.jpeg?v=1627945526057');
 }
