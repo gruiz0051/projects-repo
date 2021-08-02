d9bed41e9438e7e8aa4596e43da140b64374932b
Glitch (p5-js-template-cssi-2020) <none>
2021-08-02 23:21:47 +0000

    🌛💪 Checkpoint ./script.js:19167394/652 ./.glitch-assets:19167394/413

--- script.js
+++ script.js
@@ -6,15 +6,30 @@
           keyCode,pipes,circle,frameCount,key, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize *
 */
 let imgPosition,img;
+let turPosX = 300;
+let turPosY = 300;
 function setup() {
   createCanvas(640, 480);
-  imgPosition = { x: 250, y: 250 };
 }
 
 function draw() {
   background(0)
- image(imgPosition.x, imgPosition.y, 70)
+ image(img,145,155,150,100);
 }
 function preload() {
-  img = loadImage('https://cdn.glitch.com/d125e789-8ed6-42c4-8d94-84907c5535a6%2Fcat%20head.jpeg?v=1627945526057');
+  img = loadImage("https://cdn.glitch.com/d125e789-8ed6-42c4-8d94-84907c5535a6%2Fcat%20head.png?v=1627945997503")
+}
+class Dog{
+	constructor(xSpd, ySpd){
+		this.x = turPosX;
+		this.y = turPosY;
+		this.xSpd = 12*xSpd;
+		this.ySpd = 12*ySpd;
+	}
+	
+	showSelf(){
+		stroke(230, 255, 0);
+		fill(230, 255, 0, 135);
+		ellipse(this.x, this.y, 10);
+	}
 }
