66ae46e3a9398ba55cc39932dfb8e4d18d9cae43
Glitch (p5-js-template-cssi-2020) <none>
2021-08-02 22:58:01 +0000

    🚞🏭 Checkpoint ./script.js:19167394/2501

--- script.js
+++ script.js
@@ -3,121 +3,27 @@
 /* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
           rect, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, 
           mouseX, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, 
-          keyCode,pipes,frameCount,key, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
-let bird;
-let pipes = [];
-
-
-function Bird() {
-  this.y = height / 2;
-  this.x = 64;
-
-  this.gravity = 0.7;
-  this.lift = -12;
-  this.velocity = 0;
-
-  this.show = function() {
-    fill(255);
-    ellipse(this.x, this.y, 32, 32);
-  };
-
-  this.up = function() {
-    this.velocity += this.lift;
-  };
-
-  this.update = function() {
-    this.velocity += this.gravity;
-    // this.velocity *= 0.9;
-    this.y += this.velocity;
-
-    if (this.y > height) {
-      this.y = height;
-      this.velocity = 0;
-    }
-
-    if (this.y < 0) {
-      this.y = 0;
-      this.velocity = 0;
-    }
-  };
-}
-
+          keyCode,pipes,frameCount,key, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize *
+*/
 
 function setup() {
   createCanvas(640, 480);
-  bird = new Bird();
-  //pipes.push(new Pipe());
 }
 
 function draw() {
-  background(0);
-
-  /*for (var i = pipes.length - 1; i >= 0; i--) {
-    pipes[i].show();
-    pipes[i].update();
-
-    if (pipes[i].hits(bird)) {
-      console.log('HIT');
-    }
-
-    if (pipes[i].offscreen()) {
-      pipes.splice(i, 1);
-    }
-  }*/ 
-
-  bird.update();
-  bird.show();
-
-  if (frameCount % 75 == 0) {
-    //pipes.push(new Pipe());
-  }
+  background(255,0,0)
+}
+class Cat{
+  constructor(){
+  this.size = 10;
+    this.d = 30;
+    this.x = 30;
+    this.y = 30;
 }
 
-function keyPressed() {
-  if (key == ' ') {
-    bird.up();
-    //console.log("SPACE");
+  showSelf(){
+		noStroke();
+		fill(0);
+		ellipse(this.x, this.y, this.d );
   }
 }
-/* function Pipe() {
-  this.spacing = 175;
-  this.top = random(height / 6, (3 / 4) * height);
-  this.bottom = height - (this.top + this.spacing);
-  this.x = width;
-  this.w = 80;
-  this.speed = 6;
-
-  this.highlight = false;
-
-  this.hits = function(bird) {
-    if (bird.y < this.top || bird.y > height - this.bottom) {
-      if (bird.x > this.x && bird.x < this.x + this.w) {
-        this.highlight = true;
-        return true;
-      }
-    }
-    this.highlight = false;
-    return false;
-  };
-
-  this.show = function() {
-    fill(255);
-    if (this.highlight) {
-      fill(255, 0, 0);
-    }
-    rect(this.x, 0, this.w, this.top);
-    rect(this.x, height - this.bottom, this.w, this.bottom);
-  };
-
-  this.update = function() {
-    this.x -= this.speed;
-  };
-
-  this.offscreen = function() {
-    if (this.x < -this.w) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-} */
